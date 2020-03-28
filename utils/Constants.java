db15bc7632e62128ed973650c72415649a36f764
Rachanagangadhar <43091117+Rachanagangadhar@users.noreply.github.com>
2020-03-28 14:58:13 +0530

    utils

---
+++ utils/Constants.java
@@ -0,0 +1,9 @@
+package utils;
+
+public class Constants {
+    public static final int NO_OF_TASKS = 400; // number of Cloudlets;
+    public static final int NO_OF_DATA_CENTERS = 1; // number of Datacenters
+    public static final int POPULATION_SIZE = 50; // Number of Particles.
+    public static final int NO_OF_VMS = 40;//虚拟机个数
+    public static final int NO_OF_Iterations=50;//迭代次数
+}
---
+++ utils/GenerateMatrices.java
@@ -0,0 +1,169 @@
+package utils;
+
+import org.cloudbus.cloudsim.Vm;
+import org.cloudbus.cloudsim.lists.VmList;
+
+import java.io.*;
+import java.util.List;
+import java.util.Random;
+
+public class GenerateMatrices {
+    private static double[][] commMatrix, execMatrix;//传输转移消耗矩阵，执行时间矩阵
+    private File commFile = new File("CommunicationTimeMatrix.txt");
+    private File execFile = new File("ExecutionTimeMatrix.txt");
+    private String filePath = "/home/rachana/Downloads/cloudsim-task-scheduling-master/cloudlets.txt";
+
+    public GenerateMatrices() {
+        commMatrix = new double[Constants.NO_OF_TASKS][Constants.NO_OF_VMS];
+        execMatrix = new double[Constants.NO_OF_TASKS][Constants.NO_OF_VMS];
+        try {
+            if (commFile.exists() && execFile.exists()) {
+                readCostMatrix();
+            } else {
+                initCostMatrix();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Matrix initialization based on existing virtual machine list
+    public GenerateMatrices(List<Vm> vmlist){
+        this.vmlist = vmlist;
+
+
+        commMatrix = new double[Constants.NO_OF_TASKS][Constants.NO_OF_VMS];
+        execMatrix = new double[Constants.NO_OF_TASKS][Constants.NO_OF_VMS];
+        try {
+            if (commFile.exists() && execFile.exists()) {
+                readCostMatrix();
+            } else {
+                initCostMatrix(filePath);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void initCostMatrix() throws IOException {
+        System.out.println("Initializing new Matrices...");
+        BufferedWriter commBufferedWriter = new BufferedWriter(new FileWriter(commFile));
+        BufferedWriter execBufferedWriter = new BufferedWriter(new FileWriter(execFile));
+
+        for (int i = 0; i < Constants.NO_OF_TASKS; i++) {
+            for (int j = 0; j < Constants.NO_OF_VMS; j++) {
+                commMatrix[i][j] = Math.random() * 600 + 20;
+                execMatrix[i][j] = Math.random() * 500 + 10;
+                commBufferedWriter.write(String.valueOf(commMatrix[i][j]) + ' ');
+                execBufferedWriter.write(String.valueOf(execMatrix[i][j]) + ' ');
+            }
+            commBufferedWriter.write('\n');
+            execBufferedWriter.write('\n');
+        }
+        commBufferedWriter.close();
+        execBufferedWriter.close();
+    }
+
+    private void readCostMatrix() throws IOException {
+        System.out.println("Reading the Matrices...");
+        BufferedReader commBufferedReader = new BufferedReader(new FileReader(commFile));
+
+        int i = 0, j = 0;
+        do {
+            String line = commBufferedReader.readLine();
+            for (String num : line.split(" ")) {
+                commMatrix[i][j++] = new Double(num);
+            }
+            ++i;
+            j = 0;
+        } while (commBufferedReader.ready());
+
+
+        BufferedReader execBufferedReader = new BufferedReader(new FileReader(execFile));
+
+        i = j = 0;
+        do {
+            String line = execBufferedReader.readLine();
+            for (String num : line.split(" ")) {
+                execMatrix[i][j++] = new Double(num);
+            }
+            ++i;
+            j = 0;
+        } while (execBufferedReader.ready());
+    }
+
+    public static double[][] getCommMatrix() {
+        return commMatrix;
+    }
+
+    public static double[][] getExecMatrix() {
+        return execMatrix;
+    }
+
+    //General public method
+    public double[][] getcommMatrix() {
+        return commMatrix;
+    }
+
+    public double[][] getexecMatrix(){
+        return execMatrix;
+    }
+
+    //Method for generating task initialization matrix based on samples
+    private void initCostMatrix(String filePath) throws IOException
+    {
+        @SuppressWarnings("resource")
+        BufferedReader br= new BufferedReader(new InputStreamReader(new FileInputStream(filePath)));
+        String data = null;
+        int index=0;
+        double[] LengthGroup = new double[Constants.NO_OF_TASKS];
+        while ((data = br.readLine()) != null)
+        {
+            System.out.println(data);
+            String[] taskLength=data.split("\t");//tasklength[i]是任务执行的耗费（指令数量）
+            for(int j=0;j<taskLength.length;j++){
+                LengthGroup[index+j] = Double.parseDouble(taskLength[j]);
+                if((index+j+1)==Constants.NO_OF_TASKS)
+                {
+                    br.close();
+                    initMatrix(LengthGroup);
+                    return;
+                }
+            }
+            //20 cloudlets each line in the file cloudlets.txt.
+            index+=taskLength.length;
+        }
+    }
+
+    private void initMatrix(double[] LengthGroup) throws IOException
+    {
+        System.out.println("Initializing new Matrices...");
+        BufferedWriter commBufferedWriter = new BufferedWriter(new FileWriter(commFile));
+        BufferedWriter execBufferedWriter = new BufferedWriter(new FileWriter(execFile));
+        Random rm = new Random();
+        for (int i = 0; i < Constants.NO_OF_TASKS; i++) {
+            for (int j = 0; j < Constants.NO_OF_VMS; j++) {
+                commMatrix[i][j] = Calculator.div(VmList.getById(vmlist,j).getSize(),VmList.getById(vmlist, j).getBw());
+                execMatrix[i][j] = Calculator.div(LengthGroup[rm.nextInt(Constants.NO_OF_TASKS)],VmList.getById(vmlist, j).getMips());
+//                execMatrix[i][j] = Calculator.div(LengthGroup[i],VmList.getById(vmlist, j).getHost().getTotalAllocatedMipsForVm(VmList.getById(vmlist, j)));
+                commBufferedWriter.write(String.valueOf(commMatrix[i][j]) + ' ');
+                execBufferedWriter.write(String.valueOf(execMatrix[i][j]) + ' ');
+            }
+            commBufferedWriter.write('\n');
+            execBufferedWriter.write('\n');
+        }
+        commBufferedWriter.close();
+        execBufferedWriter.close();
+    }
+
+    private List<Vm> vmlist;
+    public void SetVmList(List<Vm> vmlist)
+    {
+        this.vmlist = vmlist;
+    }
+
+    public List<Vm> GetVmList()
+    {
+        return vmlist;
+    }
+}
